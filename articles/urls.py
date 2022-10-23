a34750a75bcb43685ecbbed19265ffaecb67171f
tanh <tanhaeuser@gmail.com>
2022-10-23 23:41:51 +0200

    continueing chapter

--- articles/urls.py
+++ articles/urls.py
@@ -1,7 +1,16 @@
 from django.urls import path
 
-from articles.views import ArticleListView 
+from articles.views import(
+    ArticleListView,
+    ArticleDetailView,
+    ArticleUpdateView,
+    ArticleDeleteView,
+) 
+
 
 urlpatterns = [
-    path("", ArticleListView.as_view(), name = "article_list"), 
+    path("<int:pk>/", ArticleDetailView.as_view(), name = "article_detail"),
+    path("<int:pk>/", ArticleUpdateView.as_view(), name = "article_edit"),
+    path("<int:pk>/", ArticleDeleteView.as_view(), name = "article_delete"),
+    path("", ArticleListView.as_view(), name = "article_list"),
     ]
